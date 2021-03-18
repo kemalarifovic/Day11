62dbaa818701636ffb21c7ddadafe2ea96da9fba
Kemal Arifovic <gigalab@gmail.com>
2021-03-18 15:15:00 +0100

    Splitting Array

--- ex11/src/index.js
+++ ex11/src/index.js
@@ -2,7 +2,7 @@
 function splitArrayInGroups(arr, n) {
   var newArr = [];
   for (var i = 0; i < arr.length; i += n) {
-      newArr.push(arr.slice(i, i+n));
+    newArr.push(arr.slice(i, i + n));
   }
   return newArr;
 }
@@ -10,4 +10,9 @@ function splitArrayInGroups(arr, n) {
 // Only change code above this line
 
 console.log(splitArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 3));
+console.log(splitArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
+
 module.exports = splitArrayInGroups;
